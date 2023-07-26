5692a9c2dfbb25a7a157d47184dd6297e0f8510a
Henry C <henry.junkmail100@gmail.com>
2023-07-26 21:35:02 +0100

    created a script that started calling the functions

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#
+from functions import make_val_dataset
+from preprocessing import BrainTumorDataset
+from model import tumour_unet
+import os
+import torch
+from torchvision.transforms import Compose, ToTensor, Normalize
+from pathlib import Path
+
+training_path = Path('F:/Users\Henry/Coding/Tumour Segmentation/archive/Training')
+data_folder= Path('F:/Users/Henry/Coding/Tumour Segmentation/archive/')
+
+make_val_dataset(training_path, data_folder, 0.2)
+
+transform = Compose([ToTensor(), Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+train_dataset = BrainTumorDataset(os.path.join(data_folder, 'Training'), transform=transform)
+val_dataset = BrainTumorDataset(os.path.join(data_folder, 'val'), transform=transform)
+test_dataset = BrainTumorDataset(os.path.join(data_folder, 'Testing'), transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
