7b847786464f4fedd6ab304837a1e6da0f494c69
Henry Crosswell <henrycrosswell@PD1071.local>
2023-07-30 13:45:09 +0100

    testing folders

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,54 +1,58 @@
 import torch
-from model import TumorUNet
+import torch.nn as nn
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from segment_tumour import CNNModel, TumourSegmentationTrainer, test_model, BrainTumourDataset
 
-def test_tumour_unet_output_shape():
-    # Create a UNet model with 1 input channel and 4 output channels (for segmentation)
-    model = TumorUNet(in_channels=1, out_channels=4)
-    
-    # Create a random input tensor with the specified shape (batch_size=1, channels=1, height=128, width=128)
-    input_tensor = torch.rand(1, 1, 128, 128)
-    
-    # Forward pass through the model
-    output = model(input_tensor)
-    
-    # Check the output shape
-    assert output.shape == torch.Size([1, 4, 128, 128])
+def test_CNNModel():
+    num_classes = 4
+    dropout_rate = 0.2
 
-def test_tumour_unet_encoding():
-    # Create a UNet model with 1 input channel and 4 output channels (for segmentation)
-    model = TumorUNet(in_channels=1, out_channels=4)
-    
-    # Create a random input tensor with the specified shape (batch_size=1, channels=1, height=128, width=128)
-    input_tensor = torch.rand(1, 1, 128, 128)
-    
-    # Forward pass through the model to get the intermediate encoding
-    encoding = model.encoder(input_tensor)
-    
-    # Check the encoding shape after the encoder part
-    assert encoding.shape == torch.Size([1, 64, 64, 64])
+    model = CNNModel(num_classes=num_classes, dropout_rate=dropout_rate)
 
-def test_tumour_unet_decoding():
-    # Create a UNet model with 1 input channel and 4 output channels (for segmentation)
-    model = TumorUNet(in_channels=1, out_channels=4)
-    
-    # Create a random input tensor with the specified shape (batch_size=1, channels=1, height=128, width=128)
-    input_tensor = torch.rand(1, 1, 128, 128)
-    
-    # Forward pass through the model to get the output after the decoder part
-    output = model(input_tensor)
-    
-    # Check the output shape after the decoder part
-    assert output.shape == torch.Size([1, 4, 128, 128])
+    # Test the model architecture
+    x = torch.randn(1, 1, 224, 224)
+    output = model(x)
+    assert output.size() == (1, num_classes)
 
-def test_tumour_unet_forward():
-    # Create a UNet model with 1 input channel and 4 output channels (for segmentation)
-    model = TumorUNet(in_channels=1, out_channels=4)
-    
-    # Create a random input tensor with the specified shape (batch_size=1, channels=1, height=128, width=128)
-    input_tensor = torch.rand(1, 1, 128, 128)
-    
-    # Forward pass through the model
-    output = model(input_tensor)
-    
-    # Check the output shape after the forward pass
-    assert output.shape == torch.Size([1, 4, 128, 128])
+    # Test the dropout rate
+    for module in model.features:
+        if isinstance(module, nn.Dropout2d):
+            assert module.p == dropout_rate
+
+def test_TumourSegmentationTrainer(sample_data_folder):
+    # Sample hyperparameters for testing
+    batch_size = 32
+    dropout_rate = 0.2
+    learning_rate = 0.001
+    num_epochs = 2
+
+    # Create dummy datasets for training, validation, and testing
+    transform = ToTensor()
+    train_dataset = BrainTumourDataset(sample_data_folder, transform=transform)
+    val_dataset = BrainTumourDataset(sample_data_folder, transform=transform)
+    test_dataset = BrainTumourDataset(sample_data_folder, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    # Initialize the model, criterion, and optimizer
+    num_classes = 4
+    model = CNNModel(num_classes=num_classes, dropout_rate=dropout_rate)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Create the trainer and perform training
+    trainer = TumourSegmentationTrainer(model, train_loader, val_loader, test_loader, criterion, optimizer, 'cpu')
+    trainer.train(num_epochs=num_epochs)
+
+    # Test the model on the test dataset
+    accuracy = test_model(trainer.model, test_loader, 'cpu')
+
+    # Ensure that the test accuracy is within a reasonable range (0 to 1)
+    assert 0.0 <= accuracy <= 1.0
+
+if __name__ == '__main__':
+    test_CNNModel()
+    test_TumourSegmentationTrainer()
